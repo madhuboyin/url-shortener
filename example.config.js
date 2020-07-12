c8326d8bc3e4955f05b70a451b2b05a2b044e2d5
Kurnianto Trilaksono <kurniantotrilaksono@amartha.com>
2020-07-12 22:50:15 +0700

    fix: fix view

---
+++ example.config.js
@@ -0,0 +1,17 @@
+example.config = {
+    my_environment: {
+        "server": {
+            "host": "localhost",
+            "port": 8000
+        },
+        "database": {
+            "username": "myuser",
+            "password": "mypass",
+            "host": "127.0.0.0",
+            "port": 5432,
+            "name": "postgres"
+        }
+    },
+}
+
+module.exports = example.config;
